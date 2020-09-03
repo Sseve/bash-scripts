de4d778d3c622f65346278dbb71f5063a276fdea
Sseve <Sseve@github.com>
2020-09-03 08:58:01 +0000

    ..

---
+++ learning/loop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# 遍历数字区间
+for i in {1..5}
+do
+    echo $i
+done
+
+# +++++++
+for j in $(seq -f "%03g" 1 5)
+do
+    echo $j
+done
+
+# ++++++++
+# c syntax
+
+for((i=0;i<10;i++));do
+    echo $i
+done
