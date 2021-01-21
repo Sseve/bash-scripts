06e79cea78d63eb920848779304a2db3e72d56dd
Sseve <41677305+Sseve@users.noreply.github.com>
2021-01-21 22:42:51 +0800

    Update flock_script.sh
    lock script

--- sundries/flock_script.sh
+++ sundries/flock_script.sh
@@ -12,16 +12,25 @@
 } 3<>lock.tmp
 echo aa
 
-:<<!
-# 方式二: 给脚本加锁
-exec 6>lock.tmp
-flock -xn 6
-[ $? -eq 1 ] && echo "This script is running, please wait!" && exit
-# do your task here.
-echo "successd."
-echo "do something..."
-sleep 100
-flock -u 6
-exec 6>$-
-echo aaa
-!
+## 方式二: 给脚本加锁
+#exec 6>lock.tmp
+#flock -xn 6
+#[ $? -eq 1 ] && echo "This script is running, please wait!" && exit
+## do your task here.
+#echo "successd."
+#echo "do something..."
+#sleep 100
+#flock -u 6
+#exec 6>$-
+#echo aaa
+
+## 方式三: 给脚本加锁
+#if [ $(ps aux | grep ttest.sh | grep -v grep | wc -l) -gt 2 ];then
+#    echo "ttest.sh is running"
+#    exit
+#fi
+#ttest(){
+#    echo "test"
+#    sleep 100
+#}
+#ttest
